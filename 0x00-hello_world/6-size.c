304c1e44bb5132be8cf3885882ceb3f02fc09d73
Oycctem <Ousstemm@gmail.com>
2023-08-10 09:48:08 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+*main - entry point or the starting point of a c program's execution
+*Printf function used to a print a string
+*Return: always(0)
+*/
+int main(void)
+{
+	printf("Size of a chat: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
