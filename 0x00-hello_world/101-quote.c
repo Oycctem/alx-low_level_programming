1fc2c2faa57668d8ff8ebb153c7bfe0f9aa91aeb
Oycctem <Ousstemm@gmail.com>
2023-08-10 10:48:07 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - entry point or the starting point of a c program's execution
+*write function used to a print a string
+*Return: retuning (1)
+*/
+int main(void)
+{
+write(1, "and and that piece of art is useful\" - Dora Korpar, 2015-10-19", 61);
+return (1);
+}
