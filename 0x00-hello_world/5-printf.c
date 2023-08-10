90a4ea826db5f01649ff4b646979d71d1d6b26f7
Oycctem <Ousstemm@gmail.com>
2023-08-10 09:38:28 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - entry point or the starting point of a c program's execution
+*Printf function used to a print a string
+*Return: always(0)
+*/
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
