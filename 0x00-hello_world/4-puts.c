99bceddfdea25c2c888564b3072e5608bb58c726
Oycctem <Ousstemm@gmail.com>
2023-08-10 05:09:41 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-
-int main (void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
-}
+#include<stdio.h>
+/* main function is the starting point of a c program's execution */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0); }
