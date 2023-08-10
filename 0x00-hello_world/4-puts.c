1120b4d10c9acb548bc221507dff5f5fd8ee3fbf
Oycctem <Ousstemm@gmail.com>
2023-08-10 08:47:03 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
-/* main function is the starting point of a c program's execution */
+#include <stdio.h>
+
+/**
+*main - Entry point or starting point of a c program's execution.
+*Return: always(0)
+*/
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-return (0); }
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
