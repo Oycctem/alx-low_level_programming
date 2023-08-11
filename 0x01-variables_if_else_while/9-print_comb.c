a4412020b7d57c635512cd3049550f61c30369c8
Oycctem <Ousstemm@gmail.com>
2023-08-11 12:20:09 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main - entry point or the starting point of a c program's execution
+*putchar function used to a print a char
+*Return: retuning (1)
+*/
+int main(void)
+{
+	int zero_to_nine;
+
+		for (zero_to_nine = '0'; zero_to_nine <= '9'; ++zero_to_nine)
+		{
+			putchar(zero_to_nine);
+			if (lowercase != '0')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+
+		return (0);
+}
