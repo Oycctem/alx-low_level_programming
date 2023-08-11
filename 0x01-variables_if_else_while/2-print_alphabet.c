548467bdd23a552ef76e286812662d6a919bd29b
Oycctem <Ousstemm@gmail.com>
2023-08-11 10:07:55 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry point or the starting point of a c program's execution
+*putchar function used to a print a char
+*Return: retuning (1)
+*/
+int main(void)
+{
+	const char *A_to_Z = "abcdefghijklmnopqrstuvwxyz"
+
+		for (int i = 0; A_to_Z[i] != '\0'; i++)
+		{
+			putchar(A_to_Z[i]);
+		}
+	putchar('\n')
+
+		return (0);
+}
