86129d17c5ccfdad812cbd2f4fc1b768f8075750
Oycctem <Ousstemm@gmail.com>
2023-08-11 12:22:03 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,7 @@ int main(void)
 		for (zero_to_nine = '0'; zero_to_nine <= '9'; ++zero_to_nine)
 		{
 			putchar(zero_to_nine);
-			if (lowercase != '0')
+			if (zero_to_nine != '0')
 			{
 				putchar(',');
 				putchar(' ');
