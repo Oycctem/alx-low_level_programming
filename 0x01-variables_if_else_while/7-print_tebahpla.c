6447702a80e851bed418ef92e0909a44a5fd90bd
Oycctem <Ousstemm@gmail.com>
2023-08-11 10:58:09 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry point or the starting point of a c program's execution
+*putchar function used to a print a char
+*Return: retuning (1)
+*/
+int main(void)
+{
+	char Z_to_A;
+
+		for (Z_to_A = 'z'; Z_to_A <= 'a'; --Z_to_A)
+		{
+			putchar(Z_to_A);
+		}
+	putchar('\n');
+
+		return (0);
+}
