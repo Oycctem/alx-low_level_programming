b9718f06dea17bfbaeebbdbd6cd1d7c25ba0bd41
Oycctem <Ousstemm@gmail.com>
2023-08-15 09:39:02 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - generates and prints the multiplication table.
+ */
+void times_table(void)
+{
+	int up = 10;
+	int down = 10;
+	int u = 0;
+
+	while (u < up)
+	{
+		int d = 0;
+
+		while (d < down)
+		{
+			int table = u * d;
+
+			if (table < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + table);
+			}
+			else
+			{
+				_putchar('0' + table / 10);
+				_putchar('0' + table % 10);
+			}
+			if (d < down - 1)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			d++;
+		}
+		_putchar('$');
+		_putchar('\n');
+
+		u++;
+	}
+}
