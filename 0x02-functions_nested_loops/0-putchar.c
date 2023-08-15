3b3b65cbfeee97ceea89b46e26b74b2c058d4f4c
Oycctem <Ousstemm@gmail.com>
2023-08-15 03:04:44 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - entry point or the starting point of a c program's execution
+*Printf function used to a print a string
+*Return: always(0)
+*/
+
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}
