fd4cdad88185c567dbc636fa75aedd28696e9490
Oycctem <Ousstemm@gmail.com>
2023-08-15 07:48:36 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+include "main.h"
+/**
+ * jack_bauer - prints every minute of a day
+ * _putchar function used to print a char
+ * Return: returning nothing(void)
+ */
+void jack_bauer(void)
+{
+	for (int hour = 0; hour < 24; hour++)
+	{
+		for (int min = 0; min < 60; min++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + min / 10);
+			_putchar('0' + min % 10);
+			_putchar('\n');
+		}
+	}
+}
