f522c759a0e30bfb2055e01d0cbda81553e93adc
Oycctem <Ousstemm@gmail.com>
2023-08-15 04:47:52 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lower case from a to z
+*_putchar function used to a print a char
+*Return: retuning nothing (void)
+*/
+
+void print_alphabet_x10(void)
+{
+	char a_to_z;
+
+	for (a_to_z = 'a'; a_to_z <= 'z'; ++a_to_z)
+	{
+		_putchar(a_to_z);
+	}
+	_putchar('\n');
+}
