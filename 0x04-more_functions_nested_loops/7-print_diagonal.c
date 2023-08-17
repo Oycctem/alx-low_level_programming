cb47a10e6d459b99e125f8317101d9089bb82013
Oycctem <Ousstemm@gmail.com>
2023-08-17 15:58:02 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,11 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int x;
+	int i, x;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			for (x = 0; x < i; x++)
 			{
