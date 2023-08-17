074cd45873c642299d3bf3e743788e1ce50e33d6
Oycctem <Ousstemm@gmail.com>
2023-08-17 08:31:28 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *_putchar: prints a char
+ *Return: returning 0
+ */
+
+int print_numbers(void)
+{
+	char 0_to_9
+
+		for (0_to_9 = '0'; 0_to_9 <= '9'; ++0_to_9)
+		{
+			_putchar(0_to_nine);
+		}
+	_putchar('\n')
+}
