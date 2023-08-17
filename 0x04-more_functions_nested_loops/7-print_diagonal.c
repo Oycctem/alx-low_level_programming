ab9316cd33c9540b25272398456598c93e25e8fd
Oycctem <Ousstemm@gmail.com>
2023-08-17 15:26:47 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_diagonal - draws a diagonal line
+ *_putchar: function that prints a char
+ *@n: the number of diagonal lines
+ *Return: Returns 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int x;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (x = 0; x < i; x++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+	_putchar('\n');
+}
+}
