ff57fd7440a657218b5d0cef60582a8478795742
Oycctem <Ousstemm@gmail.com>
2023-08-17 15:54:01 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,14 +16,13 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (x = 0; x < i; x++)
 			{
 				_putchar(' ');
 			}
-	_putchar('\\');
+	_putchar(92);
 	_putchar('\n');
 	}
-}
 }
