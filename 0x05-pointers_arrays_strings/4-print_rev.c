293e67fbd7df2a4fcd727eab6216d9741a68aad1
Oycctem <Ousstemm@gmail.com>
2023-08-21 06:19:19 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,14 @@
 void print_rev(char *s)
 {
 	int n;
+	int i;
 
-	for (n = 0; s[n] != '\0'; n--)
+	for (n = 0; s[n] != '\0'; n++)
+	{
+	}
+	for (i = n - 1; i >= 0; i--)
 {
-	_putchar(s[n]);
+	_putchar(s[i]);
 }
 _putchar('\n');
 }
