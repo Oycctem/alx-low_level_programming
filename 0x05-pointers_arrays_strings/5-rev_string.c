351a47db535eb0ae7e206d8783a2baba99561c6f
Oycctem <Ousstemm@gmail.com>
2023-08-21 07:14:52 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string
+ *@s: string
+ *Return: returns 0
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	int beggining = 0;
+	int ending = len - 1;
+
+	while (beggining < ending)
+	{
+		char a = s[beggining];
+
+		s[beggining] = s[ending];
+		s[ending] = a;
+
+		beggining++;
+		ending--;
+	}
+}
