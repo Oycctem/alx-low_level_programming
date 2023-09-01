c6aaaa6fa574170b2bbca414a8c94cf9aacb3126
Oycctem <Ousstemm@gmail.com>
2023-09-01 10:18:39 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,15 @@
  *main - entry point or the starting point of a c program's execution
  *@argc: int
  *@argv: char
- *Return: returns nothing
+ *Return: returns 0
  */
 int main(int argc, char *argv[])
 {
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
 	printf("%s\n", argv[0]);
+	}
+	return (0);
 }
