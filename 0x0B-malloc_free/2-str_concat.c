daacb26562028b5c91528f9060f3d324084bb578
Oycctem <Ousstemm@gmail.com>
2023-09-05 13:04:55 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ *str_concat - function used to merge 2 strings
+ *@s1: char
+ *@s2: char
+ *Return: returns x
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, n = 0, a = 0;
+	char x;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		n++;
+	for (i = 0; s2[i] != '\0'; i++)
+		a++;
+
+	x = malloc(sizeof(char) * (n + a) + 1);
+	if (x == NULL)
+	{
+		return ('\0');
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		x[i] = s1[i];
+	for (i = 0; s2[i] != '\0'; i++)
+		x[n + i] = s2[i];
+	return (x);
+}
