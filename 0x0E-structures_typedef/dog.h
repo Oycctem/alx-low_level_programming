d27509a29a4948e2a46eddbd0d118703af9cdd40
Oycctem <Ousstemm@gmail.com>
2023-09-11 11:37:47 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ *struct dog - structure
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: name of the dog's owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
