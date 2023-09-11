cafd1cbf0e70c6aa78c7f750072de1dfed46ed7d
Oycctem <Ousstemm@gmail.com>
2023-09-11 14:01:01 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_dog - function that frees dogs
+ *@d: structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
