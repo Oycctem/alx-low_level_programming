75e5fbeb3922b3758ce195a536b913d14469ce6b
Oycctem <Ousstemm@gmail.com>
2023-09-21 14:19:17 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *free_list - function that frees a list_t list
+ *@head: the first node
+ *Return: returns nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *n;
+
+	while (head)
+	{
+		n = head->next;
+		free(head->str);
+		free(head);
+		head = next;
+	}
+}
