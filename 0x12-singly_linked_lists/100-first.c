912671345c4680b48f0802801b12dc5921e1068e
Oycctem <Ousstemm@gmail.com>
2023-09-22 18:39:01 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ *main - entry point
+ *Return: nothing
+ */
+void __attribute__ ((constructor)) main()
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
