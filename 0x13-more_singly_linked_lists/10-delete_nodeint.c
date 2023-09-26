10623574f2144732ebca8ca35004bb69389adee2
Oycctem <Ousstemm@gmail.com>
2023-09-26 15:41:49 -0700

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(i);
 		return (1);
 	}
-	for (a = 0; a < index -1; a++)
+	for (a = 0; a < index - 1; a++)
 	{
 		if (!i || !(i->next))
 		{
