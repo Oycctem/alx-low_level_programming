93d6ce62eeda5f90986b80be0afc9496d1aa6ce9
Oycctem <ousstemm@gmail.com>
2023-11-09 08:55:40 -0800

    task 9

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newNode, *current = *h;
 	unsigned int count = 0;
 
-	if (h == NULL || newNode == NULL)
+	if (!h)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
