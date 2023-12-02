d2b9f746716bce603174b37021039f4c41e28323
Oycctem <ousstemm@gmail.com>
2023-12-01 17:56:29 -0800

    task 1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_table.h"
+/**
+ *hash_djb2 - function that implements the djb2 algorithm
+ *@str: string used to generate hash value
+ *Return: returns the hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str);
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
